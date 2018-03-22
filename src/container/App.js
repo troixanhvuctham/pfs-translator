e05a78f982e0afdd14f360ef9e118139d481681b
huyqbtran <huyqbtran@kms-technology.com>
2018-03-22 15:45:57 +0700

    Translator App.

--- src/container/App.js
+++ src/container/App.js
@@ -1,28 +1,35 @@
 import React, {Component, Fragment} from 'react';
 import './App.css';
-import AppBar from 'material-ui/AppBar';
-import {Label, Button, Input, FormGroup, Col, Row} from 'reactstrap';
+import {
+    Label,
+    Button,
+    Input,
+    FormGroup,
+    Col,
+    Card,
+    CardHeader,
+    CardBody
+} from 'reactstrap';
 import GoogleTranslate from 'google-translate';
 import Select from 'react-select';
+import 'bootstrap/dist/css/bootstrap.css';
 class App extends Component {
-    constructor() {
-        super();
+
+    constructor(props) {
+        super(props);
         this.state = {
             exportFileName: "",
-            fileChosen: null,
-            fileContent: "",
-            key: new Array(),
-            listLanguage:[],
+            listLanguage: [],
             targetLanguage: "zh",
-        }
+            desFile: ""
+        };
 
+        this.key = [];
+        this.fileChosen = null;
         this.translator = GoogleTranslate('AIzaSyDWNvub5X6qVwk8VBqKyvurHGskLGMIJPc');
-        this.handleChooseFile = this.handleChooseFile.bind(this);
-        this.handleChangeLanguage = this.handleChangeLanguage.bind(this);
-        this.convertFile = this.convertFile.bind(this);
     }
-    translate(Object) {
-        console.log("OBJECT",Object)
+
+    translate = (Object) => {
         return new Promise(((resolve, reject) => {
             this.translator.translate(Object, this.state.targetLanguage, (err, result) => {
                 if (err) reject(err);
@@ -30,105 +37,137 @@ class App extends Component {
                 this.downloadFile(result);
             });
         }));
-    }
-    handleChooseFile(event){
-        console.log("Running handle choose file...")
+    };
+
+    handleChooseFile = (event) => {
         let files = event.target.files;
+        this.fileChosen = files[0];
         this.setState({
-           fileChosen: files[0],
-        });
-    }
-    convertFile(){
-        console.log("Running convert file ...")
-        let fileReader = new FileReader();
-        fileReader.onload = function (e) {
-            this.setState({fileContent:fileReader.result});
-            let map = this.changeToMap(fileReader.result);
-            this.translate(Array.from(map));
-        }.bind(this)
-        fileReader.readAsText(this.state.fileChosen);
-    }
-    handleChangeLanguage(language){
-        console.log("Language was select: ", language.value)
+            desFile: files[0].name.split(".").join("_" + this.state.targetLanguage + "."),
+        })
+    };
+
+    convertFile = () => {
+        const fileReader = new FileReader();
+        fileReader.onload = (e) => {
+            let values = this.getValues(fileReader.result);
+            this.translate(values);
+        };
+        fileReader.readAsText(this.fileChosen);
+    };
+
+    handleChangeLanguage = (language) => {
+        let desFile = "";
+        if(this.fileChosen){
+            desFile = this.fileChosen.name.split(".").join("_" + language.value + ".");
+        }
         this.setState({
             targetLanguage: language.value,
+            desFile: desFile,
         });
+    };
 
-    }
-    changeToMap(text){
-        let lines = text.split("\n");
-        let map = new Map();
-        let key = [];
-        lines.forEach((element)=>{
-            let coupleVal = element.split(" = ");
-            map.set(coupleVal[0],coupleVal[1]);
-            key.push(coupleVal[0])
-            this.setState({
-                key : key
-            })
+    handleChangeDestination = (event) => {
+        this.setState({
+            desFile: event.target.value,
         })
-        return map.values();
-    }
-    downloadFile(values){
+    };
+    getValues = (text) => {
+        let lines = text.split("\n");
+        let values = [];
+        let key = [];
+
+        lines.forEach((element) => {
+            if(element.trim()!==""){
+                /(.*) = (.*)$/gmu.exec(element);
+                key.push(RegExp.$1);
+                values.push(RegExp.$2);
+            };
+        });
+        this.key = key;
+        return values;
+    };
+
+    downloadFile = (values) => {
         {
-            console.log("VALUE", values)
-            let listString = "";
-            for(let i = 0;i<this.state.key.length;i++){
-                listString = listString + this.state.key[i] + " = " + values[i].translatedText+"\r\n"
+            let listString = [];
+
+            for (let i = 0; i < this.key.length; i++) {
+                listString.push(`${this.key[i]} = ${values[i].translatedText}`);
             }
+            const text = listString.join("\n");
+
             let element = document.createElement("a");
-            let file = new Blob([listString], {type: 'text/plain'});
+            let file = new Blob([text], {type: 'text/plain'});
             element.href = URL.createObjectURL(file);
-            element.download = "myFile.txt";
+
+            element.download = this.state.desFile;
             element.click();
         }
-    }
-    componentDidMount(){
-        this.translator.getSupportedLanguages('en',function(err, languageCodes) {
-            console.log("LANGUAGE", languageCodes)
+    };
+
+    componentDidMount() {
+        this.translator.getSupportedLanguages('en', (err, languageCodes) => {
             let componentList = [];
             languageCodes.map((component) => {
-                componentList.push({ value : component.language, label : component.name})
+                componentList.push({value: component.language, label: component.name})
             });
             this.setState({
                 listLanguage: componentList,
             })
-        }.bind(this));
+        });
+    };
+
+    render() {
+        return (
+            <Fragment>
+                <div>
+                    <header className="App-header">
+                        <h1 className="align-content-center">Welcome</h1>
+                    </header>
+                    <Card>
+                        <CardHeader className="font-weight-bold">
+                            File Translator
+                        </CardHeader>
+                        <CardBody>
+                            <FormGroup row>
+                                <Col xs="12" md="2">
+                                    <Label>File Selector:</Label>
+                                </Col>
+                                <Col xs="12" md="3">
+                                    <Input type="file" onChange={this.handleChooseFile}/>
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Col xs="12" md="2">
+                                    <label>Language: {this.state.language}</label>
+                                </Col>
+                                <Col xs="12" md="3">
+                                    <Select options={this.state.listLanguage} onChange={this.handleChangeLanguage} simpleValue
+                                            multi/>
+                                </Col>
+                            </FormGroup>
+
+                            <FormGroup row>
+                                <Col xs="12" md="2">
+                                    <Label>Destination File:</Label>
+                                </Col>
+                                <Col xs="12" md="3">
+                                    <Input onChange={this.handleChangeDestination} value={this.state.desFile} />
+                                </Col>
+                            </FormGroup>
+                            <FormGroup row>
+                                <Col xs="12" md="5">
+                                    <label>{this.state.exportFileName}</label>
+                                    <Button className="float-right" color="success"
+                                            onClick={this.convertFile}>Export</Button>
+                                </Col>
+                            </FormGroup>
+                        </CardBody>
+                    </Card>
+                </div>
+            </Fragment>
+        );
     }
-  render() {
-    return (
-      <Fragment>
-        <AppBar
-          title="PFS Translator"
-          iconClassNameRight="muidocs-icon-navigation-expand-more"
-        />
-
-        <div>
-          <header className="App-header">
-            <h1 className="App-title">Welcome</h1>
-          </header>
-          <FormGroup row>
-            <Label>File Selector</Label>
-            <Input type="file" onChange={this.handleChooseFile} />
-            <Input type="text" onChange={this.handleChooseFile} />
-            <label>Language: {this.state.language}</label>
-              <Select options = {this.state.listLanguage} onChange = {this.handleChangeLanguage}/>
-            <Button>Choose Language</Button>
-          </FormGroup>
-          <FormGroup>
-            <Label>File Destination</Label>
-            <Input type="file"/>
-            <Input type="text" onChange={this.handleChooseFile} />
-          </FormGroup>
-          <FormGroup>
-              <label>{this.state.exportFileName}</label>
-            <Button onClick={this.convertFile}>Export</Button>
-          </FormGroup>
-        </div>
-
-      </Fragment>
-    );
-  }
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './container/App';
 import registerServiceWorker from './registerServiceWorker';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 ReactDOM.render(
-  <MuiThemeProvider>
     <App/>
-  </MuiThemeProvider>,
-  document.getElementById('root'));
+,  document.getElementById('root'));
 registerServiceWorker();
