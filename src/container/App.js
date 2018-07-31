a5461383254da8f16a071748c96f5e13cae393f3
huyqbtran <huyqbtran@kms-technology.com>
2018-07-31 17:27:10 +0700

    add tensorflow

--- src/container/App.js
+++ src/container/App.js
@@ -1,31 +1,24 @@
-import React, {PureComponent, Fragment} from 'react';
+import React, {Component, Fragment} from 'react';
 import './App.css';
 import {
-    Label,
-    Button,
-    Input,
-    FormGroup,
-    Col,
-    Card,
-    CardHeader,
-    CardBody
+    Label, Button, Input, FormGroup, Col, Card, CardHeader, CardBody
 } from 'reactstrap';
 import GoogleTranslate from 'google-translate';
 import Select from 'react-select';
-class App extends PureComponent {
+import * as tf from '@tensorflow/tfjs';
+
+class App extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            exportFileName: "",
-            listLanguage: [],
-            targetLanguage: "zh",
-            desFile: ""
+            exportFileName: "", listLanguage: [], targetLanguage: "zh", desFile: ""
         };
 
         this.key = [];
         this.fileChosen = null;
         this.translator = GoogleTranslate('AIzaSyDWNvub5X6qVwk8VBqKyvurHGskLGMIJPc');
+        this.num = new Float32Array([21]);
     }
 
     translate = (obj) => {
@@ -64,8 +57,7 @@ class App extends PureComponent {
             desFile = `${list[0]}_${language.value}.${list[1]}`;
         }
         this.setState({
-            targetLanguage: language.value,
-            desFile,
+            targetLanguage: language.value, desFile,
         });
     };
 
@@ -110,63 +102,97 @@ class App extends PureComponent {
         this.translator.getSupportedLanguages('en', (err, languageCodes) => {
             let componentList = [];
             languageCodes.map((languageCode) => {
-                componentList.push({value: languageCode.language, label: languageCode.name})
+                return componentList.push({value: languageCode.language, label: languageCode.name})
             });
             this.setState({
                 listLanguage: componentList,
             })
         });
+
+        // Define a model for linear regression.
+        const model = tf.sequential();
+        model.add(tf.layers.dense({units: 1, inputShape: [1]}));
+
+        // Prepare the model for training: Specify the loss and the optimizer.
+        model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
+
+        // Generate some synthetic data for training.
+        const xs = tf.tensor2d([1, 2, 3, 4,5,6,7,8,9,0], [10, 1]);
+        const ys = tf.tensor2d([1, 2, 3, 4,5,6,7,8,9,0], [10, 1]);
+
+        // Train the model using the data.
+        let result;
+        let num;
+        model.fit(xs, ys, {epochs: 500}).then(() => {
+            // Use the model to do inference on a data point the model hasn't seen before:
+            result = model.predict(tf.tensor2d([5], [1, 1]));
+            model.predict(tf.tensor2d([1], [1, 1])).print();
+            model.predict(tf.tensor2d([2], [1, 1])).print();
+            model.predict(tf.tensor2d([3], [1, 1])).print();
+            model.predict(tf.tensor2d([4], [1, 1])).print();
+            let r = model.getWeights();
+            console.log(r[0].dataSync(), r[1].dataSync());
+            console.log("#################")
+            num = result.dataSync()
+            console.log(num)
+            this.num = num;
+        });
     };
 
     render() {
-        return (
-            <Fragment>
-                <header className="text-lg-center App-header">
-                    <span className="align-content-center">File Translator Web App</span>
-                </header>
-                <Card>
-                    <CardHeader className="font-weight-bold">
-                        File Translator
-                    </CardHeader>
-                    <CardBody>
-                        <FormGroup row>
-                            <Col xs="12" md="2">
-                                <Label>File Selector:</Label>
-                            </Col>
-                            <Col xs="12" md="3">
-                                <Input type="file" onChange={this.handleChooseFile}/>
-                            </Col>
-                        </FormGroup>
-                        <FormGroup row>
-                            <Col xs="12" md="2">
-                                <label>Language: {this.state.language}</label>
-                            </Col>
-                            <Col xs="12" md="3">
-                                <Select options={this.state.listLanguage} onChange={this.handleChangeLanguage}
-                                        simpleValue
-                                        multi/>
-                            </Col>
-                        </FormGroup>
+        return (<Fragment>
+              <header className="text-lg-center App-header">
+                  <span className="align-content-center">File Translator Web App</span>
+              </header>
+              <Card>
+                  <CardHeader className="font-weight-bold">
+                      File Translator
+                  </CardHeader>
+                  <CardBody>
+                      <FormGroup row>
+                          <Col xs="12" md="2">
+                              <Label>File Selector:</Label>
+                          </Col>
+                          <Col xs="12" md="3">
+                              <Input type="file" onChange={this.handleChooseFile}/>
+                          </Col>
+                      </FormGroup>
+                      <FormGroup row>
+                          <Col xs="12" md="2">
+                              <label>Language: {this.state.language}</label>
+                          </Col>
+                          <Col xs="12" md="3">
+                              <Select options={this.state.listLanguage} onChange={this.handleChangeLanguage}
+                                      simpleValue
+                                      multi/>
+                          </Col>
+                      </FormGroup>
 
-                        <FormGroup row>
-                            <Col xs="12" md="2">
-                                <Label>Destination File:</Label>
-                            </Col>
-                            <Col xs="12" md="3">
-                                <Input onChange={this.handleChangeDestination} value={this.state.desFile}/>
-                            </Col>
-                        </FormGroup>
-                        <FormGroup row>
-                            <Col xs="12" md="5">
-                                <label>{this.state.exportFileName}</label>
-                                <Button className="float-right" color="success"
-                                        onClick={this.convertFile}>Export</Button>
-                            </Col>
-                        </FormGroup>
-                    </CardBody>
-                </Card>
-            </Fragment>
-        );
+                      <FormGroup row>
+                          <Col xs="12" md="2">
+                              <Label>Destination File:</Label>
+                          </Col>
+                          <Col xs="12" md="3">
+                              <Input onChange={this.handleChangeDestination} value={this.state.desFile}/>
+                          </Col>
+                          <Col xs="12" md="2">
+                              <Label>Result:</Label>
+                          </Col>
+                          <Col xs="12" md="3">
+                              <Input onChange={this.handleChangeDestination} value={this.num}/>
+                          </Col>
+                      </FormGroup>
+                      <FormGroup row>
+                          <Col xs="12" md="5">
+                              <label>{this.state.exportFileName}</label>
+                              <Button className="float-right" color="success"
+                                      onClick={this.convertFile}>Export</Button>
+                          </Col>
+                      </FormGroup>
+                  </CardBody>
+              </Card>
+          </Fragment>);
     }
 }
+
 export default App;
